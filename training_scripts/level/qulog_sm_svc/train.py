6781b581385110957cc9b9e7ec0b46e27e08ec69
Alexander Acker <alexander.acker@tu-berlin.de>
2021-07-29 20:58:18 +0200

    Updated linguistic model. Fixed scripts with relative paths.

--- training_scripts/level/qulog_sm_svc/train.py
+++ training_scripts/level/qulog_sm_svc/train.py
@@ -26,9 +26,8 @@ def preprocessing(features, targets, nlp, label2id, buffer=True):
         print("Loading token embeddings from existing buffer")
         token_embeddings = load_pickle(token_embeddings_file)
     else:
-        trf_docs = get_docs(nlp, features)
-        # token_embeddings = get_trf_embeddings(trf_docs)
-        token_embeddings = get_small_web_embeddings(trf_docs)
+        docs = get_docs(nlp, features)
+        token_embeddings = get_small_web_embeddings(docs)
         if buffer:
             store_pickle(token_embeddings, token_embeddings_file)
     token_embeddings = np.asarray(token_embeddings)
@@ -47,16 +46,37 @@ def preprocessing(features, targets, nlp, label2id, buffer=True):
     return token_embeddings, target_ids
 
 
+def sub_sample(data, num):
+    levels = data.log_level.unique()
+    sub_sampled = []
+    for l in levels:
+        l_data = data[data["log_level"] == l]
+        n = num
+        if len(l_data) < n:
+            sub_sampled.append(l_data)
+            n = len(l_data)
+        else:
+            sub_sampled.append(l_data.sample(n=n))
+        print("Adding {} logs with level {}".format(n, l))
+    return pd.concat(sub_sampled)
+
+    
+label2id = {"info": 0, "debug": 0, "trace": 0, "warning": 1, "error":1, "exception": 1, "critical": 1}
+id2label = {0: "info", 1: "error"}
+
 # Adjust if needed
 data = pd.read_csv("./training_data_log_level_pred.csv")
-features, targets = data.values[:, 0], data.values[:, 1]
+
+data = data[data['log_level'].isin(label2id)]
+data["log_level"] = data["log_level"].apply(lambda x: "warning" if x == "warn" else x)
+data = sub_sample(data, 5000)
+
+print("Dataset size: {}".format(len(data)))
+features, targets = data.values[:, 1], data.values[:, 5]
 
 # This should not be changes. Later, copy the model to the respective directory 
 # if you want to publish it. Check the readme first.
 model_file_path = "./model"
-
-label2id = {"info": 0, "error":1}
-id2label = {0:"info", 1:"error"}
 
 model = make_pipeline(StandardScaler(), SVC(gamma="auto", kernel="rbf", verbose=True))
 nlp = spacy.load("en_core_web_sm")
